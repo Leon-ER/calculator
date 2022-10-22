585ab98703ecdbd71284183fb0bd5b1dd3086133
Leon-ER <leonerdesi2004@gmail.com>
2022-10-22 17:53:48 -0400

    Completed most of the JS part and HTML

--- javascript.js
+++ javascript.js
@@ -1,1 +1,48 @@
+const buttons = document.querySelectorAll('button');
+const output = document.querySelector('.result');
+const outputTop = document.querySelector('.equasion');
 
+buttons.forEach((button) =>{
+  button.addEventListener('click', (e) =>{
+    const input = e.target.dataset.value;
+    if(input === '='){
+      if(outputTop.innerText.includes('/')){
+        const splitValues = outputTop.innerHTML.split('/')
+        operate('/', splitValues[0], splitValues[1]);
+      }
+      if(outputTop.innerText.includes('+')){
+        const splitValues = outputTop.innerHTML.split('+')
+        operate('+', splitValues[0], splitValues[1]);
+      }
+      if(outputTop.innerText.includes('-')){
+        const splitValues = outputTop.innerHTML.split('-')
+        operate('-', splitValues[0], splitValues[1]);
+      }
+      if(outputTop.innerText.includes('*')){
+        const splitValues = outputTop.innerHTML.split('*')
+        operate('*', splitValues[0], splitValues[1]);
+      }
+    }
+    outputTop.innerText += input; 
+    if(input === 'DEL'){
+      outputTop.innerText = '';
+      output.innerText = '';
+    }
+  })
+})
+function operate(operators,value1,value2){
+  const num1 = (Number(value1));
+  const num2 = (Number(value2));
+  if(operators == '/'){
+   output.innerText= num1 / num2
+  }
+  if(operators == '+'){
+    output.innerText=  num1 + num2
+  }
+  if(operators == '*'){
+    output.innerText=  num1 * num2
+  }
+  if(operators == '-'){
+    output.innerText=  num1 - num2
+  }
+}
